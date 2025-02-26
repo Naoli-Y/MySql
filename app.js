ba3aba2b78f87068cbe1fefb0fc3816e7ca4893d
Lisa <naoliyisak@gmail.com>
2025-02-26 09:37:08 +0300

    up and running

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const app = express();
 const mysql = require("mysql2");
 
 
+app.get("/", (req, res) => res.send("up and running..."));
+
+
+
+app.listen(2024, () => 
+console.log("listening and running on http://localhost:2024")
+);
